cbd289f50d635adf103986e3105ffc8675933f43
sahulhameedsld <sahulhameedsld@gmail.com>
2026-07-29 13:02:15 +0530

    Dashboard settings and subscription update

--- server.js
+++ server.js
@@ -15,16 +15,22 @@ const ProjectGroup = require("./models/ProjectGroup");
 const ProjectGroupMessage = require("./models/ProjectGroupMessage");
 const Razorpay = require("razorpay");
 
-const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET
-});
-
-console.log("KEY :", process.env.RAZORPAY_KEY_ID);
-console.log(
-  "SECRET :",
-  process.env.RAZORPAY_KEY_SECRET ? "FOUND" : "NOT FOUND"
-);
+const Razorpay = require("razorpay");
+let razorpay = null;
+if (
+    process.env.RAZORPAY_KEY_ID &&
+    process.env.RAZORPAY_KEY_SECRET
+) {
+    razorpay = new Razorpay({
+        key_id: process.env.RAZORPAY_KEY_ID,
+        key_secret: process.env.RAZORPAY_KEY_SECRET
+    });
+    console.log("✅ Razorpay Enabled");
+} else {
+    console.log("❌ Razorpay Keys Missing");
+}
+console.log("KEY =", process.env.RAZORPAY_KEY_ID);
+console.log("SECRET =", process.env.RAZORPAY_KEY_SECRET ? "FOUND" : "NOT FOUND");
 
 const fs = require("fs");
 const path = require("path");
